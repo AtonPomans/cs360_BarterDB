592b8d6fdfded95db7dc966c4d96819f21d83e19
Ashton Pomerantz <ashtonpomerantz.com>
2025-03-24 18:17:17 -0700

    added sql for barter_post for the database schema

--- sql/database_schema.sql
+++ sql/database_schema.sql
@@ -15,6 +15,21 @@ CREATE TABLE items (
     item_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     name VARCHAR(255),
-    description TEXT,
+/*    description TEXT, */
     FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE barter_post (
+    post_id INT AUTO_INCREMENT PRIMARY KEY,
+    poster_id INT NOT NULL,
+    partner_id INT,
+    offered_item INT NOT NULL,
+    requested_item INT NOT NULL,
+    offered_quantity INT DEFAULT 1,
+    requested_quantity INT DEFAULT 1,
+    status ENUM('open', 'closed') DEFAULT 'open',
+    FOREIGN KEY (poster_id) REFERENCES users(user_id),
+    FOREIGN KEY (partner_id) REFERENCES users(user_id),
+    FOREIGN KEY (offered_item) REFERENCES items(item_id),
+    FOREIGN KEY (requested_item) REFERENCES items(item_id)
 );
